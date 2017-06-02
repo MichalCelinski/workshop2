dd2a63b2d379a5c0d36f4f081cbceb357b45fe02
Adrian Stawiski <adrian.stawiski@gmail.com>
2017-06-02 15:50:40 +0000

    pyk

--- workshop_2.py
+++ workshop_2.py
@@ -11,22 +11,55 @@ class User:
     def save(self):
         db_connection = connect(user='root', password='admin', host='127.0.0.1', database='workshop_db')
         cursor = db_connection.cursor()
-        cursor.execute('INSERT INTO Users(email, username, hashed_password) VALUES (%s, %s, %s);',
-                       (self.email, self.username, self.password))
+        if self.id:
+            cursor.execute('UPDATE Users SET email=%s, username=%s, hashed_password=%s WHERE id=%s',
+                           (self.email, self.username, self.password, self.id))
+        else:
+            cursor.execute('INSERT INTO Users(email, username, hashed_password) VALUES (%s, %s, %s);',
+                           (self.email, self.username, self.password))
         cursor.close()
         db_connection.commit()
         db_connection.close()
-        return 'dodano do bazy'
 
     @staticmethod
     def load_by_email(email):
         db_connection = connect(user='root', password='admin', host='127.0.0.1', database='workshop_db')
         cursor = db_connection.cursor()
         cursor.execute('SELECT * FROM Users WHERE email=%s;', (email,))
-        for row in cursor:
-            data = list(row)
+        data = cursor.fetchone()
+        if data is None:
+            return None
         cursor.close()
         db_connection.close()
         user = User(username=data[2], password=data[3], email=data[1])
         user.id = data[0]
         return user
+
+    def delete(self):
+
+        if self.id:
+            db_connection = connect(user='root', password='admin', host='127.0.0.1', database='workshop_db')
+            cursor = db_connection.cursor()
+            cursor.execute('DELETE FROM Users WHERE id=%s', (self.id,))
+            cursor.close()
+            db_connection.commit()
+            db_connection.close()
+            self.id = None
+        else:
+            pass
+
+    @staticmethod
+    def load_all():
+        db_connection = connect(user='root', password='admin', host='127.0.0.1', database='workshop_db')
+        cursor = db_connection.cursor()
+        cursor.execute('SELECT * FROM Users')
+        all = []
+        for row in cursor:
+            user = User(row[2], row[3], row[1])
+            user.id = row[0]
+            all.append(user)
+        cursor.close()
+        db_connection.close()
+        # for i in all:
+        #     print(i.username)
+        return all
